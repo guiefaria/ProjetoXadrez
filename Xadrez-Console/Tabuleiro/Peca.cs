c7d8eae4ffae52e60fdd37685a873f4cd9698c85
Guilherme Faria <guiefaria@gmail.com>
2019-04-03 22:36:56 -0300

    Começo implementação tabuleiro

---
+++ Xadrez-Console/Tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int Qtdovimentos { get; set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor , Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Qtdovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }
+    }
+}
---
+++ Xadrez-Console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int LinhasTabuleiro{ get; set; }
+        public int ColunasTabuleiro { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhasTabuleiro, int colunasTabuleiro)
+        {
+            LinhasTabuleiro = linhasTabuleiro;
+            ColunasTabuleiro = colunasTabuleiro;
+            pecas = new Peca[linhasTabuleiro, colunasTabuleiro];
+        }
+    }
+}
